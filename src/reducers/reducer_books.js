f671b8cddbefdb7468ee47cc0b7ba8c712bce794
murthy <dmurthy1991@gmail.com>
2017-05-16 22:30:14 -0400

    React-Redux BookList and activebook
    A BookList on aside and Active Book(selected book) in section

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,15 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 196 },
+    { title: 'Nancy Drew', pages: 162 },
+    { title: 'Lord of the rings', pages: 85 },
+    { title: 'Famous Five', pages: 1 },
+    { title: 'Hardy Boys', pages: 110 },
+    { title: 'Bhagwad Gita', pages: 100 },
+     { title: 'Not without my daughter', pages: 100 }
+
+  ];
+}
